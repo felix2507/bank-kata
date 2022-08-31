087b1b44d1acbf0a1cb3f5c10992369b1df42c57
Felix Schlosser <felix.schlosser@andrena.de>
2022-08-31 14:54:50 +0200

    Add kotlin version

---
+++ BankKataKotlin/src/main/kotlin/bankingkata/Account.kt
@@ -0,0 +1,17 @@
+package bankingkata
+
+class Account {
+
+    fun withdraw(amount: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun deposit(amount: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun printStatement() {
+        TODO("Not yet implemented")
+    }
+}
+
