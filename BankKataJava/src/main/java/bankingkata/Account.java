fb24ae68a717e7049f78b06e702fce3d76117b7c
Felix Schlosser <felix.schlosser@andrena.de>
2022-09-16 13:46:19 +0200

    Make Account throw exception in not implemented methods

--- BankKataJava/src/main/java/bankingkata/Account.java
+++ BankKataJava/src/main/java/bankingkata/Account.java
@@ -1,14 +1,19 @@
 package bankingkata;
 
+import jdk.jshell.spi.ExecutionControl;
+
 public class Account {
 
     public void withdraw(int amount) {
+        throw new RuntimeException("Not yet implemented");
     }
 
     public void deposit(int amount) {
+        throw new RuntimeException("Not yet implemented");
     }
 
     public void printStatement() {
+        throw new RuntimeException("Not yet implemented");
     }
 
 }
